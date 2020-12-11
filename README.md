06f21237dd5b6e2803047fbfd6587caa35c679af
Emanuele Pace <emanuele.10@outlook.it>
2020-12-11 11:43:42 +0000

    Upload project

---
+++ README.md
@@ -0,0 +1,19 @@
+## New elements here:
+
+**input type="color"** : <input type ="color">
+
+**CSS linear gradient** for set a gradient of color from one direction to another.
+
+**input event** for receive back the kind of input. Ex: 
+inputTag.addEventListener("input", function(){
+    console.log(inputTag.value)
+})
+
+On in-line coding we can use **oninput=** and **onclick** instead the eventListener
+
+
+**h3.textContent = body.style.background + ";";** with .textContent we can add text on one dedicated html tag. 
+Similar to innerHTMl or createTextnode.
+ 
+ 
+
---
+++ script.js
@@ -0,0 +1,44 @@
+var _ = require('lodash')
+var array = [1,2,3,4,5,6,7,8]
+console.log('answer:', _.without(array, 3));
+
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+var random = document.querySelector(".rndColor")
+
+
+css.textContent = 
+"linear-gradient(to right, " 
++ color1.value   
++ ", " 
++ color2.value 
++ ")";
+
+function setGradient() {
+	body.style.background = 
+	"linear-gradient(to right, " 
+	+ color1.value 
+	+ ", " 
+	+ color2.value 
+	+ ")";
+
+	css.textContent = body.style.background + ";";
+}
+
+function randomColor(){
+	body.style.background = 
+	"linear-gradient(to right, " 
+	+ '#'+((0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6))
+	+ ", " 
+	+ '#'+((0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6))
+	+ ")";
+
+	css.textContent = body.style.background + ";";
+
+}
+	
+color1.addEventListener("input", setGradient);
+color2.addEventListener("input", setGradient);
+random.addEventListener("click", randomColor);
